9ffd623dccdfc93c066e7de0731db7974b54ae86
Jorge Morales <jmorales113@gmail.com>
2020-01-17 18:40:11 -0800

    Functions for random lower and upper case letters

---
+++ script.js
@@ -0,0 +1,7 @@
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
