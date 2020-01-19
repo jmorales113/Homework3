b894a2c3b5f664b35ae1b5e2107f08e900996f0c
Jorge Morales <jmorales113@gmail.com>
2020-01-18 23:04:18 -0800

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Homework3
+
+The purpose of this homework was to create a generated random password for the user with the use of HTML and CSS powered by Javascript. 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
@@ -5,3 +6,15 @@ function getRandomUpper() {
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomCharacter() {
+
+const characters = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+    return characters[Math.floor(Math.random() * characters.length)];
+
+}
+
