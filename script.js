9271c2259486e0f0f3d81d5c46eaa3342e5f12e9
Jorge Morales <jmorales113@gmail.com>
2020-02-02 11:03:18 -0800

    Added variables for slider.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+var passWordCharacters = "!#$%&()*+,-./:;<=>?@[]\^_`{|}~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lengthName = document.getElementById("lengthName")
+var sliderLength = document.getElementById("sliderLength")
+function generatePassword() {
+    var password = "";
+    var passwordLength = 128;
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor((Math.random()*passWordCharacters.length)+0);
+        password+= passWordCharacters.charAt(randomNumber);
+        console.log(randomNumber)
+    }
+ 
+    document.getElementById("text-box").innerHTML = password
 }
 
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+sliderLength.onclick = function(){
+    lengthName.innerHTML = "Length:" + sliderLength.value
+    generatePassword()
 }
 
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomCharacter() {
-
-const characters = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-    return characters[Math.floor(Math.random() * characters.length)];
-
-}
-
+//html input just needs id
+// need another get element by Id
